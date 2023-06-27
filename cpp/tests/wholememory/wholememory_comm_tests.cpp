65e6f7f3fd00ced1e2101d1461dd682f8709204b
Dongxu Yang <dongxuy@nvidia.com>
2023-06-27 15:46:34 +0800

    fix comm test

--- cpp/tests/wholememory/wholememory_comm_tests.cpp
+++ cpp/tests/wholememory/wholememory_comm_tests.cpp
@@ -50,7 +50,6 @@ TEST(WholeMemoryCommTest, CommunicatorFunctions)
     EXPECT_EQ(cudaSetDevice(rank), cudaSuccess);
     wholememory_comm_t wm_comm1 = create_communicator_by_pipes(pipes, rank, world_size);
     EXPECT_EQ(wm_comm1->comm_id, 0);
-    EXPECT_EQ(wholememory::destroy_all_communicators(), WHOLEMEMORY_SUCCESS);
     int comm_rank = -1;
     EXPECT_EQ(wholememory::communicator_get_rank(&comm_rank, wm_comm1), WHOLEMEMORY_SUCCESS);
     EXPECT_EQ(comm_rank, rank);
@@ -58,6 +57,7 @@ TEST(WholeMemoryCommTest, CommunicatorFunctions)
     EXPECT_EQ(wholememory::communicator_get_size(&comm_size, wm_comm1), WHOLEMEMORY_SUCCESS);
     EXPECT_EQ(comm_size, world_size);
     EXPECT_EQ(wholememory::is_intranode_communicator(wm_comm1), true);
+    EXPECT_EQ(wholememory::destroy_all_communicators(), WHOLEMEMORY_SUCCESS);
 
     WHOLEMEMORY_CHECK(::testing::Test::HasFailure() == false);
   });
