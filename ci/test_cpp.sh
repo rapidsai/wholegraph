8275eb63c3b5849105f1d0350f613c6d96504443
Brad Rees <34135411+BradReesWork@users.noreply.github.com>
2023-07-13 10:37:28 -0400

    addressing comments (#39)
    Authors:
      - Brad Rees (https://github.com/BradReesWork)
    
    Approvers:
      - Don Acosta (https://github.com/acostadon)
    
    URL: https://github.com/rapidsai/wholegraph/pull/39

--- ci/test_cpp.sh
+++ ci/test_cpp.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # Copyright (c) 2022-2023, NVIDIA CORPORATION.
 
-set -e          # abort the script on error
-set -o pipefail # piped commands propagate their error
-set -E          # ERR traps are inherited by subcommands
-trap "EXITCODE=1" ERR
-EXITCODE=0
+set -euo pipefail
+
 . /opt/conda/etc/profile.d/conda.sh
 
 rapids-logger "Generate C++ testing dependencies"
@@ -35,7 +32,8 @@ rapids-mamba-retry install \
 
 rapids-logger "Check GPU usage"
 nvidia-smi
-
+EXITCODE=0
+trap "EXITCODE=1" ERR
 set +e
 
 # Run libwholegraph tests from libwholegraph-tests package
