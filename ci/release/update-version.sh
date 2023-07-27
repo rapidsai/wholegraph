33d019a0a91aca6231e116b8460ea74145deb0c8
BradReesWork <BradReesWork@users.noreply.github.com>
2023-07-27 14:15:44 -0400

    drop CURRENT_SHORT_TAG

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -24,7 +24,6 @@ CURRENT_TAG=$(git tag --merged HEAD | grep -xE '^v.*' | sort --version-sort | ta
 CURRENT_MAJOR=$(echo $CURRENT_TAG | awk '{split($0, a, "."); print a[1]}')
 CURRENT_MINOR=$(echo $CURRENT_TAG | awk '{split($0, a, "."); print a[2]}')
 CURRENT_PATCH=$(echo $CURRENT_TAG | awk '{split($0, a, "."); print a[3]}')
-CURRENT_SHORT_TAG=${CURRENT_MAJOR}.${CURRENT_MINOR}
 
 #Get <major>.<minor> for next version
 NEXT_MAJOR=$(echo $NEXT_FULL_TAG | awk '{split($0, a, "."); print a[1]}')
@@ -32,7 +31,7 @@ NEXT_MINOR=$(echo $NEXT_FULL_TAG | awk '{split($0, a, "."); print a[2]}')
 NEXT_SHORT_TAG=${NEXT_MAJOR}.${NEXT_MINOR}
 NEXT_UCX_PY_VERSION="$(curl -sL https://version.gpuci.io/rapids/${NEXT_SHORT_TAG})"
 
-echo "Preparing release $CURRENT_TAG => $NEXT_FULL_TAG and short tag => $CURRENT_SHORT_TAG"
+echo "Preparing release $CURRENT_TAG => $NEXT_FULL_TAG and short is $NEXT_SHORT_TAG"
 
 # Inplace sed replace; workaround for Linux and Mac
 function sed_runner() {
