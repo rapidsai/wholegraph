a0ef0d274bfdb1aeb60b7df1e2450a6622e7417f
Ray Douglass <3107146+raydouglass@users.noreply.github.com>
2023-09-22 11:16:07 -0400

    Update all versions to 23.10 (#71)
    Looks like when `branch-23.10`/`v23.10.00a` was created, `update-version.sh` was not in the repo, so no versions were updated.
    
    This PR just runs `update-version.sh` for `23.10.00`.
    
    Authors:
      - Ray Douglass (https://github.com/raydouglass)
    
    Approvers:
      - Jake Awe (https://github.com/AyodeAwe)
      - AJ Schmidt (https://github.com/ajschmidt8)
    
    URL: https://github.com/rapidsai/wholegraph/pull/71

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -23,7 +23,7 @@ rapids-logger "Downloading artifacts from previous jobs"
 
 CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
 PYTHON_CHANNEL=$(rapids-download-conda-from-s3 python)
-export RAPIDS_VERSION_NUMBER="23.08"
+export RAPIDS_VERSION_NUMBER="23.10"
 export RAPIDS_DOCS_DIR="$(mktemp -d)"
 
 rapids-mamba-retry install \
