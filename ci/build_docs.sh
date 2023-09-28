65f846b32c644601cb4ac4ec40501fcaf60febe0
ayodeawe <jawe@nvidia.com>
2023-09-28 14:31:15 -0500

    rm py chann

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -22,14 +22,12 @@ rapids-print-env
 rapids-logger "Downloading artifacts from previous jobs"
 
 CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
-PYTHON_CHANNEL=$(rapids-download-conda-from-s3 python)
 export RAPIDS_VERSION_NUMBER="23.12"
 export RAPIDS_DOCS_DIR="$(mktemp -d)"
 
 rapids-mamba-retry install \
   --channel "${CPP_CHANNEL}" \
-  --channel "${PYTHON_CHANNEL}" \
-  libwholegraph pylibwholegraph
+  libwholegraph
 
 rapids-logger "Build Doxygen docs"
 pushd cpp
