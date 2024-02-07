993e79864876eace0ca7e161ae6e2dec349bdb01
ptaylor <paul.e.taylor@me.com>
2024-02-07 20:31:38 +0000

    run tests from python/pylibwholegraph/pylibwholegraph

--- ci/run_pytests.sh
+++ ci/run_pytests.sh
@@ -4,6 +4,6 @@
 set -euo pipefail
 
 # Support invoking run_pytests.sh outside the script directory
-cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../
+cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../python/pylibwholegraph/pylibwholegraph/
 
-pytest --cache-clear --forked "$@" "$(python -c 'import os; import pylibwholegraph; print(os.path.dirname(pylibwholegraph.__file__))')/tests"
+pytest --cache-clear --forked "$@" tests
