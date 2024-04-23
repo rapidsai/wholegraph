260b63137df13f81a8144b34b646e58aabb9714d
Chuang Zhu <chuangz@nvidia.com>
2024-04-23 07:49:16 +0000

    support read file with multi threads and add test_wholememory_io for round-roubin read

--- python/pylibwholegraph/pylibwholegraph/tests/pylibwholegraph/test_wholememory_io.py
+++ python/pylibwholegraph/pylibwholegraph/tests/pylibwholegraph/test_wholememory_io.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2023, NVIDIA CORPORATION.
+# Copyright (c) 2019-2024, NVIDIA CORPORATION.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -55,6 +55,7 @@ def load_routine_func(
     embedding_dim,
     embedding_stride,
     storage_offset,
+    round_robin_size=0,
 ):
     wm_comm, _ = init_torch_env_and_create_wm_comm(
         world_rank, world_size, world_rank, world_size
@@ -63,10 +64,53 @@ def load_routine_func(
     data_type = wmb.WholeMemoryDataType.DtInt
     file_list = [None] * file_part_count
 
-    per_rank_entry = wmb.determine_partition_plan(embedding_entry_count, world_size)
-    rank_start_entry = min(per_rank_entry * world_rank, embedding_entry_count)
-    rank_end_entry = min(per_rank_entry * (world_rank + 1), embedding_entry_count)
+    extra_embedding_count = embedding_entry_count
+    if round_robin_size != 0:
+        first_rank_extra_embedding_entry_count = embedding_entry_count % (
+            world_size * round_robin_size
+        )
+        first_rank_extra_embedding_entry_count = min(
+            first_rank_extra_embedding_entry_count, round_robin_size
+        )
+        extra_embedding_count = (
+            embedding_entry_count
+            - embedding_entry_count % (world_size * round_robin_size)
+            + first_rank_extra_embedding_entry_count * world_size
+        )
+
+    per_rank_entry = wmb.determine_partition_plan(extra_embedding_count, world_size)
+    rank_start_entry = min(per_rank_entry * world_rank, extra_embedding_count)
+    rank_end_entry = min(per_rank_entry * (world_rank + 1), extra_embedding_count)
     rank_entry_count = rank_end_entry - rank_start_entry
+
+    if round_robin_size != 0:
+        first_rank_extra_embedding_entry_count = embedding_entry_count % (
+            world_size * round_robin_size
+        )
+        per_rank_entry_round_robin = per_rank_entry - per_rank_entry % round_robin_size
+        if first_rank_extra_embedding_entry_count < round_robin_size:
+            if world_rank == 0:
+                rank_entry_count = (
+                    per_rank_entry_round_robin + first_rank_extra_embedding_entry_count
+                )
+            else:
+                rank_entry_count = per_rank_entry_round_robin
+        else:
+            rank_entry_count = (
+                per_rank_entry_round_robin
+                - round_robin_size
+                + min(
+                    round_robin_size,
+                    max(
+                        0,
+                        (
+                            first_rank_extra_embedding_entry_count
+                            - world_rank * round_robin_size
+                        ),
+                    ),
+                )
+            )
+        rank_end_entry = rank_start_entry + rank_entry_count
 
     reference_local_tensor = cpu_embedding_tensor_base[
         rank_start_entry:rank_end_entry, :
@@ -87,22 +131,28 @@ def load_routine_func(
                 continue
             wholememory_root_tensor = wmb.create_wholememory_matrix(
                 data_type,
-                embedding_entry_count,
+                extra_embedding_count,
                 embedding_dim + storage_offset,
                 embedding_stride,
                 wm_comm,
                 mt,
                 ml,
             )
+
             wholememory_tensor = wholememory_root_tensor.get_sub_tensor(
                 [-1, storage_offset], [-1, -1]
             )
-            wholememory_tensor.from_filelist(file_list)
+            wholememory_tensor.from_filelist(file_list, round_robin_size)
             local_tensor, local_offset = wholememory_tensor.get_local_tensor(
                 torch_import_from_dlpack,
                 wmb.WholeMemoryMemoryLocation.MlDevice,
                 world_rank,
             )
+
+            if round_robin_size != 0:
+                assert local_tensor.shape[0] == per_rank_entry
+
+                local_tensor = local_tensor[:rank_entry_count]
 
             assert local_tensor.dim() == 2
             assert local_tensor.shape[0] == rank_entry_count
@@ -122,17 +172,41 @@ def load_routine_func(
 @pytest.mark.parametrize("embedding_dim", [16, 31, 33])
 @pytest.mark.parametrize("embedding_stride", [16, 32, 64])
 @pytest.mark.parametrize("storage_offset", [0, 3])
+@pytest.mark.parametrize("round_robin_size", [256, 1024, 0])
 def test_wholememory_load(
     file_part_count,
     embedding_entry_count,
     embedding_dim,
     embedding_stride,
     storage_offset,
+    round_robin_size,
 ):
     if embedding_stride < storage_offset + embedding_dim:
         pytest.skip(
             "Skipping due to embedding_stride, embedding_dim and storage_offset configuration not valid."
         )
+    if round_robin_size != 0 and storage_offset != 0:
+        pytest.skip(
+            "Skipping due to round_robin_size!=0 and storage offset !=0 , the configuration is not valid."
+        )
+    global gpu_count
+    if not gpu_count:
+        gpu_count = 1
+    extra_embedding_count = embedding_entry_count
+    if round_robin_size != 0:
+        first_rank_extra_embedding_entry_count = embedding_entry_count % (
+            gpu_count * round_robin_size
+        )
+        first_rank_extra_embedding_entry_count = min(
+            first_rank_extra_embedding_entry_count, round_robin_size
+        )
+
+        extra_embedding_count = (
+            embedding_entry_count
+            - embedding_entry_count % (gpu_count * round_robin_size)
+            + first_rank_extra_embedding_entry_count * gpu_count
+        )
+
     cpu_embedding_tensor_base = torch.randint(
         -1000000000,
         1000000000,
@@ -154,6 +228,26 @@ def test_wholememory_load(
             "%s_part_%d_of_%d" % (file_name_prefix, i, file_part_count)
         )
 
+    if round_robin_size != 0:
+        entry_per_rank = wmb.determine_partition_plan(extra_embedding_count, gpu_count)
+
+        cpu_embedding_tensor_base_extra = torch.empty(
+            (extra_embedding_count, embedding_dim), dtype=torch.int, device="cpu"
+        )
+        global_indices = torch.arange(0, embedding_entry_count, device="cpu")
+        indices_to_robin_id = global_indices // round_robin_size
+        indices_to_rank = indices_to_robin_id % gpu_count
+        indices_to_robin_id_offset = indices_to_robin_id // gpu_count
+        target_id = (
+            indices_to_rank * entry_per_rank
+            + indices_to_robin_id_offset * round_robin_size
+            + global_indices % round_robin_size
+        )
+        cpu_embedding_tensor_base_extra[target_id] = cpu_embedding_tensor_base
+
+        cpu_embedding_tensor_base = cpu_embedding_tensor_base_extra
+        cpu_embedding_tensor_base.contiguous()
+
     cpu_embedding_tensor_base = cpu_embedding_tensor_base.share_memory_()
 
     load_routine_func_partial = partial(
@@ -165,9 +259,9 @@ def test_wholememory_load(
         embedding_dim=embedding_dim,
         embedding_stride=embedding_stride,
         storage_offset=storage_offset,
+        round_robin_size=round_robin_size,
     )
 
-    global gpu_count
     multiprocess_run(gpu_count, load_routine_func_partial)
 
     for i in range(file_part_count):
