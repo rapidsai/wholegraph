b453a1dad2f559cc2fae23c1feb8ffc3b7f59ea6
linhu-nv <141609318+linhu-nv@users.noreply.github.com>
2024-04-30 18:21:52 +0800

    allow temp_memory_handler to allocate memory for multiple times (#161)
    fix to[ issue 76](https://github.com/rapidsai/wholegraph/issues/76), which allows temp_memory_handler to allocate memory for multiple times.
    
    Authors:
      - https://github.com/linhu-nv
    
    Approvers:
      - Chuang Zhu (https://github.com/chuangz0)
    
    URL: https://github.com/rapidsai/wholegraph/pull/161

--- python/pylibwholegraph/pylibwholegraph/torch_cpp_ext/wholegraph_torch_ext.cpp
+++ python/pylibwholegraph/pylibwholegraph/torch_cpp_ext/wholegraph_torch_ext.cpp
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2019-2024, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include <torch/extension.h>
 #include <torch/script.h>
 
@@ -24,6 +39,11 @@ void wrapped_destroy_output_context(int64_t output_context)
   wholegraph_torch::destroy_output_context(reinterpret_cast<void*>(output_context));
 }
 
+void wrapped_free_context_data(int64_t output_context)
+{
+  wholegraph_torch::free_context_data(reinterpret_cast<void*>(output_context), nullptr);
+}
+
 torch::Tensor get_torch_tensor_from_output_context(int64_t output_context)
 {
   auto* torch_output_context =
@@ -39,6 +59,7 @@ PYBIND11_MODULE(pylibwholegraph_torch_ext, m)
   m.def("get_stream", &wrapped_get_stream, "Get current CUDA stream.");
   m.def("create_output_context", &wrapped_create_output_context, "Create output memory context.");
   m.def("destroy_output_context", &wrapped_destroy_output_context, "Destroy output memory context.");
+  m.def("free_context_data", &wrapped_free_context_data, "Free data in output memory context.");
   m.def("get_tensor_from_context",
         &get_torch_tensor_from_output_context,
         "Get PyTorch Tensor from output memory context");
