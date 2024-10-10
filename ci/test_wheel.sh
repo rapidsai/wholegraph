326d5121a9748f83432a5cad09f19f6bd0da4af3
James Lamb <jlamb@nvidia.com>
2024-10-10 10:16:46 -0500

    revert wheel testing changes

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2023-2024, NVIDIA CORPORATION.
+# Copyright (c) 2023, NVIDIA CORPORATION.
 
 set -e          # abort the script on error
 set -o pipefail # piped commands propagate their error
@@ -7,9 +7,11 @@ set -E          # ERR traps are inherited by subcommands
 
 mkdir -p ./dist
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
-RAPIDS_PY_WHEEL_NAME="pylibwholegraph_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 python ./dist
+RAPIDS_PY_WHEEL_NAME="pylibwholegraph_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
 
-# determine pytorch source
+# echo to expand wildcard before adding `[extra]` requires for pip
+python -m pip install $(echo ./dist/pylibwholegraph*.whl)
+
 PKG_CUDA_VER="$(echo ${CUDA_VERSION} | cut -d '.' -f1,2 | tr -d '.')"
 PKG_CUDA_VER_MAJOR=${PKG_CUDA_VER:0:2}
 if [[ "${PKG_CUDA_VER_MAJOR}" == "12" ]]; then
@@ -17,22 +19,13 @@ if [[ "${PKG_CUDA_VER_MAJOR}" == "12" ]]; then
 else
   INDEX_URL="https://download.pytorch.org/whl/cu${PKG_CUDA_VER}"
 fi
-
-# echo to expand wildcard before adding `[extra]` requires for pip
-python -m pip install \
-    -v \
-    --extra-index-url "${INDEX_URL}" \
-    "$(echo ./dist/pylibwholegraph*.whl)" \
-    numpy \
-    pytest \
-    pytest-forked \
-    'torch>=2.0.0a0'
-
 RAPIDS_TESTS_DIR=${RAPIDS_TESTS_DIR:-"${PWD}/test-results"}
 RAPIDS_COVERAGE_DIR=${RAPIDS_COVERAGE_DIR:-"${PWD}/coverage-results"}
 mkdir -p "${RAPIDS_TESTS_DIR}" "${RAPIDS_COVERAGE_DIR}"
 
 rapids-logger "Installing PyTorch"
+rapids-retry python -m pip install --pre torch --index-url ${INDEX_URL}
+rapids-retry python -m pip install pytest pytest-forked numpy
 rapids-logger "pytest pylibwholegraph"
 cd python/pylibwholegraph/pylibwholegraph/tests
 python -m pytest \
