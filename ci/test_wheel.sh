9b1c1c99836de8858e61d6f4ad2eedb2f71ff397
James Lamb <jlamb@nvidia.com>
2024-10-23 12:36:39 -0500

    manage more dependencies in dependencies.yaml, declare 'numpy' runtime dependency (#230)
    While working on adding CI for `wholegraph` in the new `cugraph-gnn` repo (https://github.com/rapidsai/cugraph-gnn/pull/58), I noticed a few issues here.
    
    * `pylibwholegraph` imports `numpy` at runtime, but its wheels and conda package don't declare a runtime dependency on `numpy`
    * wheel runtime and testing dependencies are not being managed by `rapids-dependency-file-generator`
    * `wholegraph_binding` Cython code imports NumPy but doesn't use it
    * the wheel-testing CI environment is built up with a sequence of `pip install` calls instead of a single one to get all dependencies (see https://github.com/rapidsai/cugraph/pull/4701)
    
    This proposes fixes for those things.
    
    ## Notes for Reviewers
    
    ### Where is the NumPy runtime dependency coming from?
    
    https://github.com/rapidsai/wholegraph/blob/0efba33835d6e4e104b5d7101a91e0ea55a6ca53/python/pylibwholegraph/pylibwholegraph/torch/data_loader.py#L14
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
    
    URL: https://github.com/rapidsai/wholegraph/pull/230

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2023, NVIDIA CORPORATION.
+# Copyright (c) 2023-2024, NVIDIA CORPORATION.
 
 set -e          # abort the script on error
 set -o pipefail # piped commands propagate their error
@@ -9,9 +9,7 @@ mkdir -p ./dist
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 RAPIDS_PY_WHEEL_NAME="pylibwholegraph_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
 
-# echo to expand wildcard before adding `[extra]` requires for pip
-python -m pip install $(echo ./dist/pylibwholegraph*.whl)
-
+# determine PyTorch source
 PKG_CUDA_VER="$(echo ${CUDA_VERSION} | cut -d '.' -f1,2 | tr -d '.')"
 PKG_CUDA_VER_MAJOR=${PKG_CUDA_VER:0:2}
 if [[ "${PKG_CUDA_VER_MAJOR}" == "12" ]]; then
@@ -19,13 +17,18 @@ if [[ "${PKG_CUDA_VER_MAJOR}" == "12" ]]; then
 else
   INDEX_URL="https://download.pytorch.org/whl/cu${PKG_CUDA_VER}"
 fi
+
+# echo to expand wildcard before adding `[extra]` requires for pip
+python -m pip install \
+  -v \
+  --extra-index-url "${INDEX_URL}" \
+  "$(echo ./dist/pylibwholegraph_${RAPIDS_PY_CUDA_SUFFIX}*.whl)[test]" \
+  'torch>=2.0,<2.4.0a0'
+
 RAPIDS_TESTS_DIR=${RAPIDS_TESTS_DIR:-"${PWD}/test-results"}
 RAPIDS_COVERAGE_DIR=${RAPIDS_COVERAGE_DIR:-"${PWD}/coverage-results"}
 mkdir -p "${RAPIDS_TESTS_DIR}" "${RAPIDS_COVERAGE_DIR}"
 
-rapids-logger "Installing PyTorch"
-rapids-retry python -m pip install --pre torch --index-url ${INDEX_URL}
-rapids-retry python -m pip install pytest pytest-forked numpy
 rapids-logger "pytest pylibwholegraph"
 cd python/pylibwholegraph/pylibwholegraph/tests
 python -m pytest \
